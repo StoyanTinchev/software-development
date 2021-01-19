64e60ccd55c1a7d98091efc1e553525b9e0fe0e5
with <st8tinchev@gmail.com>
2021-01-19 22:13:12 +0200

    logs

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, flash, session
 from flask import render_template, redirect, url_for, request
 import os
+import logging
 
 from film import Film
 from comment import Comment
@@ -9,6 +10,19 @@ from ratings import Rating
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
+
+logFormatStr = "[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s"
+logging.basicConfig(format=logFormatStr, filename="global.log", level=logging.DEBUG)
+formatter = logging.Formatter(logFormatStr, "%m-%d %H:%M:%S")
+fileHandler = logging.FileHandler("flask-app.log")
+fileHandler.setLevel(logging.DEBUG)
+fileHandler.setFormatter(formatter)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.DEBUG)
+streamHandler.setFormatter(formatter)
+app.logger.addHandler(fileHandler)
+app.logger.addHandler(streamHandler)
+app.logger.info("Logging is set up.")
 
 
 @app.route("/")
@@ -31,10 +45,12 @@ def register():
         pass_rep = User.hash_password(request.form["psw-repeat"])
         if password == pass_rep:
             if User.find_by_username(username):
+                app.logger.info("'%s' wants to register again!", username)
                 flash("There is already a person with such a name, please choose another")
                 return render_template("register.html")
             else:
                 User(None, username, password).create()
+                app.logger.info("'%s' registered successfully!", username)
             return redirect(url_for("home"))
         else:
             flash("The passwords do not match", "info")
@@ -57,8 +73,10 @@ def login():
             session["usr"] = request.form["username"]
             if checkbox is not None:
                 session["log"] = True
+            app.logger.info("'%s' logged in successfully", user.username)
             return redirect(url_for("home"))
         else:
+            app.logger.warn("'%s' is NOT logged!", user.username)
             flash("Wrong username or password!", "info")
             return render_template("login.html")
     elif request.method == "GET":
@@ -68,6 +86,7 @@ def login():
 @app.route("/log_out")
 def log_out():
     if "usr" in session:
+        app.logger.info("'%s' logged out", session["usr"])
         session.pop("usr", None)
         if "log" in session:
             session.pop("log")
@@ -83,7 +102,7 @@ def home():
 @app.route("/profile")
 def profile():
     if "usr" not in session:
-        return redirect(url_for(go_to))
+        return redirect(url_for("go_to"))
     return render_template("films.html", films=Film.films_by_author(session["usr"]),
                            usr=session["usr"])
 
@@ -91,13 +110,14 @@ def profile():
 @app.route("/profile/add_film", methods=["GET", "POST"])
 def add_film():
     if "usr" not in session:
-        return redirect(url_for(go_to))
+        return redirect(url_for("go_to"))
     if request.method == "GET":
         return render_template("add_film.html")
     elif request.method == "POST":
         film_name = request.form["film_name"]
         film_content = request.form["film_content"]
-        Film(*(None, film_name, session['usr'], film_content)).create()
+        film = Film(*(None, film_name, session["usr"], film_content)).create()
+        app.logger.debug("Film with id '%s' is created", film.film_id)
         return redirect(url_for("home"))
 
 
@@ -113,6 +133,7 @@ def show_film(film_id):
 @app.route("/home/<int:film_id>/delete", methods=["POST"])
 def delete_film(film_id):
     film = Film.find(film_id)
+    app.logger.debug("Film with id '%s' is deleted", film_id)
     film.delete()
     return redirect(url_for("home"))
 
@@ -122,25 +143,27 @@ def new_comment():
     film = Film.find(request.form["film_id"])
     user = session["usr"]
     Comment(*(None, request.form["message"], user, film)).create()
+    app.logger.info("'%s' add a comment to the film '%s'", session["usr"], film.name)
     return redirect(url_for("show_film", film_id=film.film_id))
 
 
 @app.route("/home/<int:film_id>/edit", methods=["GET", "POST"])
 def edit_film(film_id):
     film = Film.find(film_id)
-    if request.method == 'GET':
-        return render_template('add_film.html', film=film)
-    elif request.method == 'POST':
-        film.name = request.form['film_name']
-        film.content = request.form['film_content']
+    if request.method == "GET":
+        return render_template("add_film.html", film=film)
+    elif request.method == "POST":
+        film.name = request.form["film_name"]
+        film.content = request.form["film_content"]
         film.save()
-        return redirect(url_for('show_film', film_id=film.film_id))
+        app.logger.debug("Film with id '%s' edited", film.film_id)
+        return redirect(url_for("show_film", film_id=film.film_id))
 
 
 @app.route("/ratings/")
 def ratings():
     if "usr" not in session:
-        return redirect(url_for(go_to))
+        return redirect(url_for("go_to"))
     films = Film.film_ratings()
     return render_template("ratings.html", film=films)
 
@@ -148,19 +171,20 @@ def ratings():
 @app.route("/ratings/<int:film_id>", methods=["GET", "POST"])
 def rate_film(film_id):
     current_film = Film.find(film_id)
-    if request.method == 'GET':
-        return render_template('rate.html', film=current_film)
-    elif request.method == 'POST':
-        rate = request.form['rate']
+    if request.method == "GET":
+        return render_template("rate.html", film=current_film)
+    elif request.method == "POST":
+        rate = request.form["rate"]
         try:
             if int(rate) > 5 or int(rate) < 0:
                 flash("Please insert rating between 0 and 5!")
-                return render_template('rate.html', film=current_film)
-        except Exception as err:
+                return render_template("rate.html", film=current_film)
+        except Exception:
             flash("Please insert rating between 0 and 5!")
-            return render_template('rate.html', film=current_film)
+            return render_template("rate.html", film=current_film)
         Rating.create(rate, film_id)
-    return redirect(url_for('ratings'))
+        app.logger.info("'%s' add a rating to the film '%s'", session["usr"], current_film.name)
+    return redirect(url_for("ratings"))
 
 
 if __name__ == "__main__":
